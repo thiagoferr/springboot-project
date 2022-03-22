2fe046d22660bb99608ea98ff312edad3b8498d3
Thiago Ferreira <thiago1729@gmail.com>
2022-03-22 14:47:02 -0300

    Deploy app to Heroku (FINAL)

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -30,7 +30,7 @@ public class Order implements Serializable {
 
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
-	
+
 	private Integer orderStatus;
 
 	@ManyToOne
@@ -39,10 +39,10 @@ public class Order implements Serializable {
 
 	@OneToMany(mappedBy = "id.order")
 	private Set<OrderItem> items = new HashSet<>();
-	
+
 	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
 	private Payment payment;
-	
+
 	public Order() {
 	}
 
@@ -69,7 +69,7 @@ public class Order implements Serializable {
 	public void setMoment(Instant moment) {
 		this.moment = moment;
 	}
-	
+
 	public OrderStatus getOrderStatus() {
 		return OrderStatus.valueOf(orderStatus);
 	}
@@ -95,11 +95,11 @@ public class Order implements Serializable {
 	public void setPayment(Payment payment) {
 		this.payment = payment;
 	}
-	
+
 	public Set<OrderItem> getItems() {
 		return items;
 	}
-	
+
 	public Double getTotal() {
 		double sum = 0.0;
 		for (OrderItem x : items) {
@@ -107,7 +107,7 @@ public class Order implements Serializable {
 		}
 		return sum;
 	}
-	
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
--- src/main/java/com/educandoweb/course/repositories/OrderItemRepository.java
+++ src/main/java/com/educandoweb/course/repositories/OrderItemRepository.java
@@ -1,10 +1,11 @@
 package com.educandoweb.course.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.educandoweb.course.entities.OrderItem;
-import com.educandoweb.course.entities.pk.OrderItemPK;
 
-public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemPK> {
-
+@Repository
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long>{
+	
 }
